a6513780de2e35826b3b8bc4dc63471f23af3d92
黒岩 祐 <kuroiwa.tasuku@b-architects.com>
2017-06-05 17:09:22 +0900

    files commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+const clickDoms = document.getElementById('js-command_click');
+
+function nodeAdd(event){
+  let addDom = document.createElement('span');
+  this.appendChild(addDom);
+  if(event.ctrlKey || event.metaKey){
+    addDom.innerHTML = " + command or control";
+  } else {
+    addDom.innerHTML = " click only!! ";
+  }
+}
+clickDoms.addEventListener("click",nodeAdd);
